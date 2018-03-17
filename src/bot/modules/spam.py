9fe740db4b1762ac407a21ffe88ea207b52d81ef
ictrobot <ictrobot@outlook.com>
2018-03-17 22:03:20 +0000

    Improve spam prevention: - hash attachments (sha256 or imagehash) to check if similar - store last warning time in each channel in db to prevent multiple warnings

--- src/bot/modules/spam.py
+++ src/bot/modules/spam.py
@@ -1,11 +1,37 @@
-from bot.module import *
 import datetime
+from io import BytesIO
+import hashlib
+
+import imagehash
+from PIL import Image
+from peewee import *
+
+from bot.module import *
+
+
+class AttachmentHash(Model):
+    message_id = IntegerField()
+    hash = TextField()
+    datetime = TimestampField()
+
+    class Meta:
+        database = Proxy()
+
+
+class SpamWarning(Model):
+    user_id = IntegerField()
+    channel_id = IntegerField()
+    datetime = TimestampField()
+
+    class Meta:
+        primary_key = CompositeKey('user_id', 'channel_id')
+        database = Proxy()
 
 
 class SpamModule(Module):
 
     def __init__(self, instance):
-        super().__init__(instance, "spam")
+        super().__init__(instance, "spam", db_models=[AttachmentHash, SpamWarning])
 
     async def ratelimit(self, message):
         if message.author == self.bot.user or message.channel.permissions_for(message.author).administrator:
@@ -14,17 +40,58 @@ class SpamModule(Module):
         warning_present = False
         warning_msg = "Please don't spam {}".format(message.author.mention)
         check_start = datetime.datetime.now() - datetime.timedelta(days=1)
-        async for previous_message in message.channel.history(limit=20):
-            if previous_message.created_at < check_start:
-                break
-            if previous_message.content == message.content and previous_message.author == message.author:
-                appearances += 1
-            if previous_message.content == warning_msg:
-                warning_present = True
+        if message.attachments:
+            attachments = []
+            for attachment in message.attachments:
+                f = BytesIO()
+                await attachment.save(f)
+
+                if attachment.width:
+                    img = Image.open(f)
+                    attachment_hash = imagehash.dhash(img)
+                else:
+                    m = hashlib.sha256()
+                    m.update(f.getbuffer())
+                    attachment_hash = m.hexdigest()
+
+                attachments.append(attachment_hash)
+                AttachmentHash(message_id=message.id, datetime=message.created_at, hash=attachment_hash).save(force_insert=True)
+                print(attachment.url, attachment_hash)
+                f.close()
+
+            filter = AttachmentHash.hash == attachments[0]
+            for img_hash in attachments[1:]:
+                filter |= img_hash
+
+            discord_ids = []
+            for entry in AttachmentHash.select().where(filter):
+                if entry.datetime > check_start:
+                    discord_ids.append(entry.message_id)
+
+            async for previous_message in message.channel.history(limit=50):
+                if previous_message.author == message.author and previous_message.id in discord_ids:
+                    appearances += 1
+                if previous_message.content == warning_msg:
+                    warning_present = True
+        else:
+            async for previous_message in message.channel.history(limit=25):
+                if previous_message.created_at < check_start:
+                    break
+                if previous_message.author == message.author and previous_message.content == message.content:
+                    appearances += 1
+                if previous_message.content == warning_msg:
+                    warning_present = True
+
         if appearances >= 4:
             await safe_delete(message)
             if not warning_present:
-                await message.channel.send(warning_msg)
+                spam_warning, just_created = SpamWarning.get_or_create(user_id=message.author.id, channel_id=message.channel.id, defaults={"datetime": message.created_at})
+                if just_created:
+                    await message.channel.send(warning_msg)
+                elif spam_warning.datetime + datetime.timedelta(hours=2) < message.created_at:
+                    spam_warning.datetime = message.created_at
+                    spam_warning.save()
+                    await message.channel.send(warning_msg)
             return True
 
     @add_event_handler(1001)
